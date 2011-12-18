816126b631f364c75ee2a487d10edc54f2440378
Aaron Crow <dirtyvagabond@yahoo.com>
2011-12-17 18:19:51 -0800

    add description; add groupid 'facts'

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject geolocation "1.0.0"
-  :description "FIXME: write description"
+(defproject facts/geolocation "1.0.0"
+  :description "Geolocation library for things like IP-to-city lookup."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.2.5"]
                  [org.clojure/data.json "0.1.1"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
+                     [lein-clojars "0.6.0"]]
 
 )
